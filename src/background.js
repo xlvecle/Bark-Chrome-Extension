062ba6aced9cfc84c9ce349e797e5644a578a87e
xlvecle <xlvecle@xlvecle.com>
2020-03-09 23:31:08 +0800

    add sowrd tab

--- src/background.js
+++ src/background.js
@@ -1,126 +1,59 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-	chrome.tabs.sendRequest(tab.id, {
-		method: "getSelection"
-	}, function (response) {
-		chrome.storage.sync.get({
-			default_push_content: "clipboard"
-		}, function (items) {
-			//if default is URL, push URL
-			if (items.default_push_content === "URL") {
-				console.log(response);
-				if (response != null && response.data != '') {
-					var selectedText = response.data;
-					console.log("send selected text: " + selectedText)
-					sendMsg(response.data);
-				} else {
-					console.log("send url")
-					sendUrl(tab);
-				}
-			} else if (items.default_push_content === "clipboard") {
-				// if default is clipboard, push clipboard data
-				sendClipboardData();
-			}
-		});
-	});
-});
+let MAX_SYNC_DATA_SIZE = 500;
 
-//send selected text
-function getword(info, tab) {
-	console.log("menu " + info.menuItemId + " was clicked.");
-	console.log("Word " + info.selectionText + " was clicked.");
-	sendMsg(info.selectionText, info.menuItemId);
-}
-
-//send current page url
-function sendUrl(tab) {
-	chrome.tabs.query({
-		'active': true,
-		'lastFocusedWindow': true
-	}, function (tabs) {
-		var currentUrl = tabs[0].url;
-		sendMsg(currentUrl);
-		console.log(currentUrl);
-	});
-}
-
-//send clipboard data
-function sendClipboardData() {
-	var result = '';
-	var sandbox = document.getElementById('sandbox');
-	sandbox.value = '';
-	sandbox.select();
-	if (document.execCommand('paste')) {
-		result = sandbox.value;
-	}
-	sandbox.value = '';
-	console.log("clipboard conetent: " + result);
-	sendMsg(result);
-}
-
-function sendMsg(content, full_server_url = "") {
-	chrome.storage.sync.get({
-		server_urls: []
-	}, function (items) {
-		if (items.server_urls === '' | items.server_urls.length === 0) {
-			alert("please set server_url in options!");
+chrome.browserAction.onClicked.addListener(
+	function (tab) {
+		chrome.tabs.getAllInWindow(null, function(tabs){
 			chrome.tabs.create({
 				url: "options.html"
 			});
-			// chrome.tabs.create({ 'url': 'chrome://extensions/?options=' + chrome.runtime.id });
-		} else {
-			if (full_server_url === "") {
-				full_server_url = items.server_urls[0].server_url;
-			}
-			console.log(full_server_url);
-			httpGetAsync(full_server_url + encodeURIComponent(content) + "?automaticallyCopy=1", function () {
-				var notification = new Notification("Message Sent", {
-					body: content,
-					icon: "bark_128.png"
-				});
-			});
-		};
-	});
-}
+			currentDatas = [];
+		    for (var i = 0; i < tabs.length; i++) {
+		    	if (!tabs[i].url.startsWith("chrome-extension://")) {
+		    		chrome.tabs.remove(tabs[i].id);
+		    		currentTime = new Date();
 
-function httpGetAsync(theUrl, callback) {
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.onreadystatechange = function () {
-		if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-			callback(xmlHttp.responseText);
+		    		currentData = new Object();
+		    		currentData.timeStamp = currentTime.getTime();
+		    		currentData.date = currentTime.toLocaleString();
+		    		currentData.url = tabs[i].url;
+		    		currentData.title = tabs[i].title;
+		    		currentData.favIconUrl = tabs[i].favIconUrl;
+		    		currentDatas.push(currentData);         
+		    	}
+		    }
+
+		    if (currentDatas.length==0) {
+		    	console.log("tmpty");
+		    	return
+		    }
+
+		    chrome.storage.sync.get( {
+		    	tabData: [],
+		    }, function (data) {
+		    	console.log(data.tabData);
+		    	data.tabData.push(currentDatas);
+
+
+		    	//Limited by the space of chrome sync storage, we have to put extra data locally
+		    	if (data.tabData.length > MAX_SYNC_DATA_SIZE) {
+		    		var shiftData = data.tabData.shift();
+
+		    		chrome.storage.local.get( {
+		    			tabData: [],
+		    		}, function (local_data) {
+		    			local_data.tabData.push(shiftData);
+		    			chrome.storage.local.set({
+		    			  tabData: local_data.tabData,
+		    			});  
+		    		}
+		    		);
+
+		    	}
+		    	//save to chrome.storage
+		    	chrome.storage.sync.set({
+		    	  tabData: data.tabData,
+		    	});   
+		    }); 
+		});
 	}
-	xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-	xmlHttp.send(null);
-}
-
-function registerContextMenus() {
-	chrome.storage.sync.get({
-		server_urls: [],
-	}, function (items) {
-		console.log(items);
-		chrome.contextMenus.removeAll(function() {
-			console.log("items" + items[0]);
-			for (const it of items.server_urls) {
-				chrome.contextMenus.create({
-					title: "Push To iPhone " + it.server_name,
-					contexts: ["selection"],
-					onclick: getword,
-					id: it.server_url
-				});
-			}
-		});
-	});
-}
-
-registerContextMenus();
-
-chrome.runtime.onMessage.addListener(
-	function (request, sender, sendResponse) {
-		console.log(sender.tab ?
-			"from a content script:" + sender.tab.url :
-			"from the extension");
-		if (request.greeting == "hello")
-			registerContextMenus();
-		sendResponse({
-			farewell: "goodbye"
-		});
-	});
+);
--- src/options.js
+++ src/options.js
@@ -1,159 +1,52 @@
-var data = {
-  serverURLs: []
-}
-var data = {}
-var dataServerURLs = []
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
 
+chrome.storage.sync.get( {
+  tabData: [],
+}, function (data) {
+  fullHTML = "";
+  console.log(data);
 
-/**
- * default push content
- */
-var radios = document.getElementsByName("default_push_content");
-  for(i in radios) {
-    radios[i].onclick = function(it) {
-        data.defaultPushContent = this.value;
-    }
-}
+  for (var i = data.tabData.length-1; i>= 0; i--) {
+    // console.log(data.tabData[i]);
+    var str = `<div><hr><ul><span>SavedTime: ${data.tabData[i][0].date}</span><button class="restore" id="restore_${i}">restore</button>
+    <button class="delete" id="delete_${i}" value="${i}">delete</button>`
+    data.tabData[i].forEach(function(it) {
+      str += `<li class="url"><div><a href="${it.url}" target="_blank">${it.title}</a></li>`;
+    }); 
 
-Object.defineProperty(data, 'defaultPushContent', {
-  configurable: true,
-  get: function() {
-    return defaultPushContent;
-  },
-  set: function(value) {
-    console.log(value);
-    defaultPushContent = value;
-    document.getElementById(value).checked=true;
+    str += '</ul></div>';
 
-    //save to chrome.storage
-    chrome.storage.sync.set({
-      default_push_content: this.defaultPushContent,
-    }, function() {
-      // Update status to let user know options were saved.
-      var status = document.getElementById('status');
-      status.textContent = 'Options saved.';
-      setTimeout(function() {
-        status.textContent = '';
-      }, 200);
-    });
+    fullHTML += str;
   }
-})
-
-
-/**
- * set serverURLs
- */
-Object.defineProperty(data, 'serverURLs', {
-    configurable: true,
-    get: function() {
-      return dataServerURLs;
-    },
-    set: function(value) {
-      dataServerURLs = value;
-      var str = '<ul>';
-      value.forEach(function(it) {
-        str += '<li class="url"><u>' + it.server_name +'</u> <strong>'+ it.server_url + '</strong> <button>delete</button></li>';
-      }); 
-
-      str += '</ul>';
-      document.getElementById('urls').innerHTML = str;
-      //set delete button 
-      $("ul").on("click", "button", delete_server);
-
-      //save to chrome.storage
-      chrome.storage.sync.set({
-        server_urls: this.serverURLs,
-      }, function() {
-        // Update status to let user know options were saved.
-        chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-          console.log(response.farewell);
-        });
-
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function() {
-          status.textContent = '';
-        }, 750);
-      });
-
-    }
-})
-
-function ValidURL(str) {
-  var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
-  if(!regex .test(str)) {
-    alert("Please enter valid URL.");
-    return false;
-  } else {
-    return true;
-  }
-}
-
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  chrome.storage.sync.get({
-    server_urls: [],
-    default_push_content: "clipboard"
-  }, function(items) {
-    data.serverURLs = items.server_urls;
-    data.defaultPushContent = items.default_push_content;
-    // document.getElementById('server_url').value = items.server_urls;
-  });
-}
-
-//delete server urls
-function delete_server(e) {
-  e.preventDefault();
-  dataServerURLs.splice($(this).parent().index(), 1);
-  data.serverURLs = dataServerURLs;
-  $(this).parent().remove();
-}
-
-// Saves options to chrome.storage
-function addServer() {
-  var server_name = document.getElementById('server_name').value;
-  var server_url = document.getElementById('server_url').value;
-
-  if (ValidURL(server_url)) { //check url if valid 
-
-    //Check server if valid
-    var server_origin = server_url.substr(0, server_url.lastIndexOf("/", server_url.lastIndexOf("/") - 1));
-    httpGetAsync(server_origin + "/ping", function (httpData) {
-      if (httpData == 'error') {
-        alert('Invalid Server URL!')
-      } else {
-        httpData = JSON.parse(httpData);
-        console.log(httpData);
-        if (httpData.code === 200 && httpData.message === 'pong') {
-          console.log('Valid Server');
-          data.serverURLs.push({"server_name": server_name, "server_url": server_url});
-          //TODO ugly listen the push array change 
-          data.serverURLs = data.serverURLs
-        } else {
-          alert("Invalid Server URL!" + data.message);
-        }
+  document.getElementById('urls').innerHTML = fullHTML;
+  //set restore button 
+  var restoreButtons = document.getElementsByClassName("restore");
+  for (var i=0; i < restoreButtons.length; i++) {
+      restoreButtons[i].onclick = function (e) {
+        var links = Array.from(this.parentNode.children).filter(el=> el.tagName == "LI");
+        links.forEach(it=>
+          chrome.tabs.create({url: it.firstChild.firstChild.href})
+        )
       }
-    })
-  }
-}
+  };
 
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('add').addEventListener('click',
-    addServer);
 
-function httpGetAsync(theUrl, callback) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4) {
-          if (xmlHttp.status == 200) {
-            callback(xmlHttp.responseText);
-          } else {
-            callback('error');
-          }
-        }
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
-}
+  //set restore button 
+  var deleteButtons = document.getElementsByClassName("delete");
+  for (var i=0; i < deleteButtons.length; i++) {
+      deleteButtons[i].onclick = function (e) {
+        console.log(this.value);
+        data.tabData.remove(this.value);
+        this.parentNode.parentNode.remove();
+        //save to chrome.storage
+        chrome.storage.sync.set({
+          tabData: data.tabData,
+        });  
+      }
+  };
 
+});      
